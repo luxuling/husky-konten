6aaac6ca3a7bfd58c9a88fd84681897ea867d7c6
Antoni Saputra <antonisaputra049@gmail.com>
2024-07-17 20:38:22 +0700

    feat: test husky

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+interface IUser {
+  id: number;
+  name: string;
+}
+
+const users: IUser[] = [
+  {
+    id: 1,
+    name: "Bagus",
+  },
+  {
+    id: 2,
+    name: "Budi",
+  },
+];
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/users", (c) => {
+  c.status(200);
+  return c.json(users);
+});
+
+export default app;
